f45e93b05205cfb722d5327b39d7fc55288b6e90
mborzee <53446985+morganeborzee@users.noreply.github.com>
2020-04-23 16:12:35 -0500

    Add files via upload

---
+++ culture-general.js
@@ -0,0 +1,191 @@
+
+
+infoData = {
+   "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties":{ "name": "Caral", "group_id": 0, "startDate": [2900, "a.C."], "endDate": [1800, "a.C."], "period": [ "Precerámico", "Horizonte Temprano"], "description": "Cerro Sechín (also Sechín de las Estelas) is an archaeological site in Casma Province of Ancash Region in northern Peru. Dating to 1600 BC, the site was discovered by Peruvian archaeologists Julio C. Tello and Toribio Mejía Xesspe on July 1, 1937. Tello believed it was the capital of an entire culture, now known as the Casma/Sechin culture or Sechin complex.",
+       "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.523793,
+          -10.883654
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Chavin", "group_id": 1, "startDate": [1200, "a.C."], "endDate": [200, "d.C."],"period": [ "Periodo Inicial", "Horizonte Temprano"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.177220,
+          -9.593122 
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Paracas", "group_id": 2, "startDate": [850, "a.C."], "endDate" : [200, "a.C."], "period": ["Periodo Inicial","Horizonte Temprano"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.246790,
+          -13.869507
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Nazca", "group_id": 3, "startDate": [50, "d.C."], "endDate" : [600, "d.C."], "period": ["Horizonte Temprano"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.912825,
+          -14.912146
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Recuay", "group_id": 4, "startDate": [200, "d.C."], "endDate": [700, "d.C."], "period": ["Horizonte Temprano"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.455043,
+          -9.720347
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Mochica", "group_id": 5, "startDate": [200, "d.C."], "endDate": [850, "d.C."], "period": ["Intermedio Temprano","Horizonte Medio"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -78.661581,
+          -8.670093
+        ]
+      }
+    },
+
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Lima", "group_id": 6, "startDate": [200, "d.C."], "endDate": [750, "d.C."], "period": ["Horizonte Temprano"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.901489,
+          -12.250774
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Cajamarca", "group_id": 7, "startDate": [200, "d.C."], "endDate" :[1470, "d.C."], "period": ["Intermedio Temprano","Horizonte Medio","Intermedio Tardío"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.246790,
+          -13.869507
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Tiahuanaco", "group_id": 8, "startDate": [500, "d.C."], "endDate": [1000, "d.C."], "period": ["Intermedio Tardío"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -69.208980,
+          -16.409065
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Chachapoyas", "group_id": 9, "startDate": [400, "d.C."], "endDate" : [1470, "d.C."], "period": ["Intermedio Temprano","Horizonte Medio","Intermedio Tardío"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.871180,
+          -6.224903 
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Wari", "group_id": 10, "startDate": [550, "d.C."], "endDate": [1100, "d.C."], "period": ["Horizonte Medio"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.172889,
+          -9.342283
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Lambayeque", "group_id": 11, "startDate": [900, "d.C."], "endDate": [1350, "d.C."], "period": ["Intermedio Tardío"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.503292,
+          -6.101731
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Chimu", "group_id": 12, "startDate": [900, "d.C."], "endDate" : [1470, "d.C."], "period": ["Intermedio Tardío"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.073849,
+          -8.104936
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": { "name": "Chancay", "group_id": 13, "startDate": [1200, "d.C."], "endDate" : [1470, "d.C."], "period": ["Horizonte Tardío"], "description": "Lorem ipsum, lorem ipsjm, lorem ipsum", "thumbImage": "images/huaca_prieta.jpg", "link": "#", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.268423,
+          -11.549724
+        ]
+      }
+    }
+
+
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ culture-sites.js
@@ -0,0 +1,431 @@
+
+
+cultureSites = {
+   "type":"FeatureCollection",
+   "features":[
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Chavin",
+            "group_id":1,
+            "site_name":"Huaca Prieta",
+            "type":"Archeological Site",
+            "date":"100, 300",
+            "longitude":"-79.306949",
+            "latitude":"-7.924008",
+            "province":"La Libertad",
+            "link":"https://es.wikipedia.org/wiki/Huaca_Prieta",
+            "photo":"images/huaca_prieta.jpg",
+            "color": "#660000", 
+            "radius": "7"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.306949,
+               -7.924008
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Chavin",
+            "group_id":1,
+            "site_name":"Kotosh",
+            "type":"Archeological Site",
+            "date":"100, 300",
+            "longitude":"-76.279443",
+            "latitude":"-9.930747",
+            "province":"La Libertad",
+            "link":"https://es.wikipedia.org/wiki/Kotosh",
+            "color": "#660000", "radius": "7",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/kotosh.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -76.279443,
+               -9.930747
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Chavin",
+            "group_id":1,
+            "site_name":"Las Aldas",
+            "type":"Archeological Site",
+            "date":"100, 300",
+            "longitude":"-78.296674",
+            "latitude":"-9.701645",
+            "province":"La Libertad",
+            "link":"https://es.wikipedia.org/wiki/Las_Haldas",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/aldas.png"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -78.296674,
+               -9.701645
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Chavin",
+            "group_id":1,
+            "site_name":"Bandurria",
+            "type":"Archeological Site",
+            "date":"100, 301",
+            "longitude":"-77.587886",
+            "latitude":"-11.186705",
+            "province":"La Libertad",
+            "link":"https://es.wikipedia.org/wiki/Bandurria_(sitio_arqueol%C3%B3gico)",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/badurria.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -77.587886,
+               -11.186705
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Chavin",
+            "group_id":1,
+            "site_name":"El Paraiso",
+            "type":"Archeological Site",
+            "date":"100, 302",
+            "longitude":"-77.118409",
+            "latitude":"-11.953733",
+            "province":"La Libertad",
+            "link":"https://es.wikipedia.org/wiki/Huaca_El_Para%C3%ADso",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/el_paraiso.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -77.118409,
+               -11.953733
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Nazca",
+            "group_id":3,
+            "site_name":"Cahuachi",
+            "type":"Archeological Site",
+            "date":"100, 303",
+            "longitude":"-75.116667",
+            "latitude":"-14.818611",
+            "province":"Ica",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/huaca_prieta.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -75.116667,
+               -14.818611
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Nazca",
+            "group_id":3,
+            "site_name":"Pernil Alto",
+            "type":"Archeological Site",
+            "date":"100, 304",
+            "longitude":"-75.206",
+            "latitude":"-14.485",
+            "province":"Ica",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/kotosh.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -75.206,
+               -14.485
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Nazca",
+            "group_id":3,
+            "site_name":"Los Molinos",
+            "alternate_name":"Molino",
+            "type":"Archeological Site",
+            "date":"100, 305",
+            "longitude":"-74.953",
+            "latitude":"-14.412",
+            "province":"Ica",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/aldas.png"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -74.953,
+               -14.412
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Nazca",
+            "group_id":3,
+            "site_name":"La Muña",
+            "alternate_name":"Cerro Muña",
+            "type":"Archeological Site",
+            "date":"100, 306",
+            "longitude":"-75.213",
+            "latitude":"-14.548",
+            "province":"Ica",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/badurria.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -75.213,
+               -14.548
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Nazca",
+            "group_id":3,
+            "site_name":"Lineas de Nasca",
+            "type":"Archeological Site",
+            "date":"100, 307",
+            "longitude":"-75.134767",
+            "latitude":"-14.705824",
+            "province":"Ica",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/el_paraiso.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -75.134767,
+               -14.705824
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Nazca",
+            "group_id":3,
+            "site_name":"Ocucaje",
+            "alternate_name":"Geoglifos Ocucaje",
+            "type":"Archeological Site",
+            "date":"100, 308",
+            "longitude":"-75.642",
+            "latitude":"-14.413",
+            "province":"Ica",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/huaca_prieta.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -75.642,
+               -14.413
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"Huaca Cao Viejo",
+            "type":"Archeological Site",
+            "date":"100, 309",
+            "longitude":"-79.303447",
+            "latitude":"-7.915156",
+            "province":"La Libertad",
+            "link":"https://es.wikipedia.org/wiki/Huaca_Cao_Viejo",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/kotosh.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.303447,
+               -7.915156
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"Sipán",
+            "type":"Archeological Site",
+            "date":"100, 310",
+            "longitude":"-79.600219",
+            "latitude":"-6.798751",
+            "province":"La Libertad",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/aldas.png"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.600219,
+               -6.798751
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"Pampa Grande",
+            "type":"Archeological Site",
+            "date":"100, 311",
+            "longitude":"-79.474219",
+            "latitude":"-6.762238",
+            "province":"La Libertad",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/badurria.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.474219,
+               -6.762238
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"San José de Moro",
+            "type":"Archeological Site",
+            "date":"100, 312",
+            "longitude":"-79.438684",
+            "latitude":"-7.181054",
+            "province":"La Libertad",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/el_paraiso.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.438684,
+               -7.181054
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"Pacatnamú",
+            "type":"Archeological Site",
+            "date":"100, 313",
+            "longitude":"-79.584324",
+            "latitude":"-7.317887",
+            "province":"La Libertad",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/huaca_prieta.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.584324,
+               -7.317887
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"Huaca el brujo",
+            "type":"Archeological Site",
+            "date":"100, 314",
+            "longitude":"-79.304007",
+            "latitude":"-7.911842",
+            "province":"La Libertad",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/kotosh.jpg"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -79.304007,
+               -7.911842
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "name":"Mochica",
+            "group_id":5,
+            "site_name":"Pirámides de Moche",
+            "type":"Archeological Site",
+            "date":"100, 315",
+            "longitude":"-78.998264",
+            "latitude":"-8.141529",
+            "province":"La Libertad",
+            "color": "#660000", 
+            "radius": "7",
+            "photo":"images/aldas.png"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               -78.998264,
+               -8.141529
+            ]
+         }
+      }
+   ]
+}
